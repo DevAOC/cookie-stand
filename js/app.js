38280b1a4b51c5d0ba472cf32db3747d5d3d7dd6
AntoineCharette <antoineocharette@gmail.com>
2021-07-19 20:39:27 -0400

    fixed decimals in averages and added rendering function

--- js/app.js
+++ js/app.js
@@ -10,24 +10,18 @@ const seattle = {
   cookiesPerHour: [],
   getHourlyCust: function() {
     for (let i = 0; i < this.hours.length; i++) {
-      this.custPerHour[i] = Math.random() * (this.maxHourlyCust - this.minHourlyCust + 1) + this.minHourlyCust;
+      this.custPerHour[i] = Math.floor(Math.random() * (this.maxHourlyCust - this.minHourlyCust + 1) + this.minHourlyCust);
     }
-  },
-  sum: function(a, b) {
-    return a + b;
-  },
-  product: function(a, b) {
-    return a * b;
   },
   getHourlyCookies: function() {
     for (let i = 0; i < this.hours.length; i++) {
-      this.cookiesPerHour[i] = this.product(this.custPerHour[i], this.avgCookieSales);
+      this.cookiesPerHour[i] = Math.floor(this.custPerHour[i] * this.avgCookieSales);
     }
   },
   getCookieTotals: function() {
     let cookieTotals = 0;
     for (let i = 0; i < this.cookiesPerHour.length; i++) {
-      cookieTotals = this.sum(cookieTotals, i);
+      cookieTotals = cookieTotals + this.cookiesPerHour[i];
     }
     return cookieTotals;
   }
@@ -37,29 +31,23 @@ const tokyo = {
   hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
   minHourlyCust: 3,
   maxHourlyCust: 24,
-  avgCookieSale: 1.2,
+  avgCookieSales: 1.2,
   custPerHour: [],
   cookiesPerHour: [],
   getHourlyCust: function() {
     for (let i = 0; i < this.hours.length; i++) {
-      this.custPerHour[i] = Math.random() * (this.maxHourlyCust - this.minHourlyCust + 1) + this.minHourlyCust;
+      this.custPerHour[i] = Math.floor(Math.random() * (this.maxHourlyCust - this.minHourlyCust + 1) + this.minHourlyCust);
     }
-  },
-  sum: function(a, b) {
-    return a + b;
-  },
-  product: function(a, b) {
-    return a * b;
   },
   getHourlyCookies: function() {
     for (let i = 0; i < this.hours.length; i++) {
-      this.cookiesPerHour[i] = this.product(this.custPerHour[i], this.avgCookieSales);
+      this.cookiesPerHour[i] = Math.floor(this.custPerHour[i] * this.avgCookieSales);
     }
   },
   getCookieTotals: function() {
     let cookieTotals = 0;
     for (let i = 0; i < this.cookiesPerHour.length; i++) {
-      cookieTotals = this.sum(cookieTotals, i);
+      cookieTotals = cookieTotals + this.cookiesPerHour[i];
     }
     return cookieTotals;
   }
@@ -69,29 +57,23 @@ const dubai = {
   hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
   minHourlyCust: 11,
   maxHourlyCust: 38,
-  avgCookieSale: 3.7,
+  avgCookieSales: 3.7,
   custPerHour: [],
   cookiesPerHour: [],
   getHourlyCust: function() {
     for (let i = 0; i < this.hours.length; i++) {
-      this.custPerHour[i] = Math.random() * (this.maxHourlyCust - this.minHourlyCust + 1) + this.minHourlyCust;
+      this.custPerHour[i] = Math.floor(Math.random() * (this.maxHourlyCust - this.minHourlyCust + 1) + this.minHourlyCust);
     }
-  },
-  sum: function(a, b) {
-    return a + b;
-  },
-  product: function(a, b) {
-    return a * b;
   },
   getHourlyCookies: function() {
     for (let i = 0; i < this.hours.length; i++) {
-      this.cookiesPerHour[i] = this.product(this.custPerHour[i], this.avgCookieSales);
+      this.cookiesPerHour[i] = Math.floor(this.custPerHour[i] * this.avgCookieSales);
     }
   },
   getCookieTotals: function() {
     let cookieTotals = 0;
     for (let i = 0; i < this.cookiesPerHour.length; i++) {
-      cookieTotals = this.sum(cookieTotals, i);
+      cookieTotals = cookieTotals + this.cookiesPerHour[i];
     }
     return cookieTotals;
   }
@@ -106,24 +88,18 @@ const paris = {
   cookiesPerHour: [],
   getHourlyCust: function() {
     for (let i = 0; i < this.hours.length; i++) {
-      this.custPerHour[i] = Math.random() * (this.maxHourlyCust - this.minHourlyCust + 1) + this.minHourlyCust;
+      this.custPerHour[i] = Math.floor(Math.random() * (this.maxHourlyCust - this.minHourlyCust + 1) + this.minHourlyCust);
     }
-  },
-  sum: function(a, b) {
-    return a + b;
-  },
-  product: function(a, b) {
-    return a * b;
   },
   getHourlyCookies: function() {
     for (let i = 0; i < this.hours.length; i++) {
-      this.cookiesPerHour[i] = this.product(this.custPerHour[i], this.avgCookieSales);
+      this.cookiesPerHour[i] = Math.floor(this.custPerHour[i] * this.avgCookieSales);
     }
   },
   getCookieTotals: function() {
     let cookieTotals = 0;
     for (let i = 0; i < this.cookiesPerHour.length; i++) {
-      cookieTotals = this.sum(cookieTotals, i);
+      cookieTotals = cookieTotals + this.cookiesPerHour[i];
     }
     return cookieTotals;
   }
@@ -133,33 +109,30 @@ const lima = {
   hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
   minHourlyCust: 2,
   maxHourlyCust: 16,
-  avgCookieSale: 4.6,
+  avgCookieSales: 4.6,
   custPerHour: [],
   cookiesPerHour: [],
   getHourlyCust: function() {
     for (let i = 0; i < this.hours.length; i++) {
-      this.custPerHour[i] = Math.random() * (this.maxHourlyCust - this.minHourlyCust + 1) + this.minHourlyCust;
+      this.custPerHour[i] = Math.floor(Math.random() * (this.maxHourlyCust - this.minHourlyCust + 1) + this.minHourlyCust);
     }
-  },
-  sum: function(a, b) {
-    return a + b;
-  },
-  product: function(a, b) {
-    return a * b;
   },
   getHourlyCookies: function() {
     for (let i = 0; i < this.hours.length; i++) {
-      this.cookiesPerHour[i] = this.product(this.custPerHour[i], this.avgCookieSales);
+      this.cookiesPerHour[i] = Math.floor(this.custPerHour[i] * this.avgCookieSales);
     }
   },
   getCookieTotals: function() {
     let cookieTotals = 0;
     for (let i = 0; i < this.cookiesPerHour.length; i++) {
-      cookieTotals = this.sum(cookieTotals, i);
+      cookieTotals = cookieTotals + this.cookiesPerHour[i];
     }
     return cookieTotals;
   }
 };
 for (let i = 0; i < storeList.length; i++) {
+  
+}
+function renderStoreList(storeList) {
   
 }
